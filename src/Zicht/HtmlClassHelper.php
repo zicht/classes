8f020840b6615caa392e982da941bf71fa577306
Robert van Kempen <robert@zicht.nl>
2017-03-31 09:55:01 +0200

    Initial commit

---
+++ src/Zicht/HtmlClassHelper.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * @copyright Zicht Online
+ */
+namespace Zicht;
+
+/**
+ * Class that holds the helper functions for CSS classes.
+ */
+class HtmlClassHelper
+{
+    /**
+     * Builds a string of CSS class names based on supplied predicates.
+     *
+     * @param array ...$config
+     * @return string
+     */
+    public static function getClasses(...$config)
+    {
+        $classes = [];
+
+        foreach ($config as $argument) {
+            $classes = self::addArgumentToClasses($classes, $argument);
+        }
+
+        return join('  ', $classes);
+    }
+
+    /**
+     * Helper function that determines whether the supplied classes should
+     * be added, according to the supplied argument.
+     *
+     * @param array $classes
+     * @param string|array $argument
+     * @return array
+     */
+    private static function addArgumentToClasses($classes, $argument)
+    {
+        switch(gettype($argument))
+        {
+            case 'string':
+                array_push($classes, $argument);
+                break;
+            case 'array':
+                if (!self::hasStringKeys($argument)) {
+                    $classes = array_merge($classes, $argument);
+                    break;
+                }
+
+                foreach($argument as $class => $predicate) {
+                    if (!$predicate) {
+                        continue;
+                    }
+
+                    array_push($classes, $class);
+                }
+                break;
+
+            default:
+                break;
+        }
+
+        return $classes;
+    }
+
+
+    /**
+     * Determines whether an array has string keys.
+     *
+     * @param array $array
+     * @return bool
+     */
+    private static function hasStringKeys(array $array)
+    {
+        $keys = array_keys($array);
+
+        /**
+         * If the array keys of the keys match the keys, then the array must not be associative.
+         */
+        return array_keys($keys) !== $keys;
+    }
+}
---
+++ src/Zicht/Tests/HtmlClassHelperTest.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * @copyright Zicht Online
+ */
+namespace Zicht\Bundle\HtmldevBundle\Tests;
+
+use PHPUnit_Framework_TestCase;
+use Zicht\HtmlClassHelper;
+
+/**
+ * Tests for the HtmlClassHelperTest class.
+ */
+class HtmlClassHelperTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function getClassesSupportsSingleString()
+    {
+        $classes = HtmlClassHelper::getClasses('art-vandelay');
+
+        $this->assertEquals('art-vandelay', $classes);
+    }
+
+    /**
+     * @test
+     */
+    public function getClassesSupportsMultipleStrings()
+    {
+        $classes = HtmlClassHelper::getClasses('art-vandelay', 'kramerica');
+
+        $this->assertEquals('art-vandelay  kramerica', $classes);
+    }
+
+    /**
+     * @test
+     */
+    public function getClassesSupportsArrayWithSingleEntry()
+    {
+        $classes = HtmlClassHelper::getClasses(['art-vandelay']);
+
+        $this->assertEquals('art-vandelay', $classes);
+    }
+
+    /**
+     * @test
+     */
+    public function getClassesSupportsStringWithArray()
+    {
+        $classes = HtmlClassHelper::getClasses('art-vandelay', ['kramerica', 'kel-varnsen']);
+
+        $this->assertEquals('art-vandelay  kramerica  kel-varnsen', $classes);
+    }
+
+    /**
+     * @test
+     */
+    public function getClassesSupportsArrayWithMultipleEntries()
+    {
+        $classes = HtmlClassHelper::getClasses(['art-vandelay', 'kramerica']);
+
+        $this->assertEquals('art-vandelay  kramerica', $classes);
+    }
+
+    /**
+     * @test
+     */
+    public function getClassesSupportsObjectWithTruePredicate()
+    {
+        $classes = HtmlClassHelper::getClasses(['art-vandelay' => true]);
+
+        $this->assertEquals('art-vandelay', $classes);
+    }
+
+    /**
+     * @test
+     */
+    public function getClassesSupportsObjectWithFalsePredicate()
+    {
+        $classes = HtmlClassHelper::getClasses(['art-vandelay' => false]);
+
+        $this->assertEmpty($classes);
+    }
+
+    /**
+     * @test
+     */
+    public function getClassesSupportsObjectWithMultipleTruePredicates()
+    {
+        $classes = HtmlClassHelper::getClasses(['art-vandelay' => true, 'kramerica' => true]);
+
+        $this->assertEquals('art-vandelay  kramerica', $classes);
+    }
+
+    /**
+     * @test
+     */
+    public function getClassesSupportsObjectWithMultipleFalsePredicates()
+    {
+        $classes = HtmlClassHelper::getClasses(['art-vandelay' => false, 'kramerica' => false]);
+
+        $this->assertEmpty($classes);
+    }
+
+    /**
+     * @test
+     */
+    public function getClassesSupportsObjectWithMultipleMixedPredicates()
+    {
+        $classes = HtmlClassHelper::getClasses(['art-vandelay' => true, 'kramerica' => false, 'kel-varnsen' => true]);
+
+        $this->assertEquals('art-vandelay  kel-varnsen', $classes);
+    }
+
+    /**
+     * @test
+     */
+    public function getClassesSupportsStringAndObjectWithMultipleMixedPredicates()
+    {
+        $classes = HtmlClassHelper::getClasses('jerry', ['art-vandelay' => true, 'kramerica' => false, 'kel-varnsen' => false]);
+
+        $this->assertEquals('jerry  art-vandelay', $classes);
+    }
+
+    /**
+     * @test
+     */
+    public function getClassesDoesNotBreakOnUnsupportedTypes()
+    {
+        $classes = HtmlClassHelper::getClasses(new \stdClass(), false, 0, 1, true, null);
+
+        $this->assertEmpty($classes);
+    }
+}
